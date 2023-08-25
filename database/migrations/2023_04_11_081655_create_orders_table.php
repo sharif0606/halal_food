5ca9ac5adb89971ef8a77fdc7bfda6245ad9fed5
BIPLAB UDDIN <biplabuddin990@gmail.com>
2023-08-25 21:55:12 +0600

    update

--- database/migrations/2023_04_11_081655_create_orders_table.php
+++ database/migrations/2023_04_11_081655_create_orders_table.php
@@ -16,12 +16,14 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('db_customers')->onDelete('cascade');
+            $table->string('invoice_no')->nullable();
             $table->foreignId('billing_id')->constrained('billings')->onDelete('cascade');
             $table->decimal('sub_total', 10, 2);
             $table->decimal('discount_amount', 10, 2)->default(0);
             $table->string('cupon_code')->nullable();
             $table->decimal('shipping_charge', 10, 2)->nullable();
             $table->decimal('total', 10, 2);
+            $table->integer('paymenttype_id')->nullable();
             $table->string('status')->default(0);
             $table->timestamps();
             $table->softDeletes();
