9ccf2041e310736a904d51ab91a09d61e07aceda
BIPLAB UDDIN <biplabuddin990@gmail.com>
2023-08-25 21:38:45 +0600

    orders table updated

--- database/migrations/2023_04_11_081655_create_orders_table.php
+++ database/migrations/2023_04_11_081655_create_orders_table.php
@@ -17,11 +17,11 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('db_customers')->onDelete('cascade');
             $table->foreignId('billing_id')->constrained('billings')->onDelete('cascade');
-             $table->decimal('sub_total', 10, 2);
-             $table->decimal('discount_amount', 10, 2)->default(0);
+            $table->decimal('sub_total', 10, 2);
+            $table->decimal('discount_amount', 10, 2)->default(0);
             $table->string('cupon_code')->nullable();
             $table->decimal('shipping_charge', 10, 2)->nullable();
-             $table->decimal('total', 10, 2);
+            $table->decimal('total', 10, 2);
             $table->string('status')->default(0);
             $table->timestamps();
             $table->softDeletes();
